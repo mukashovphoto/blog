215833117d36897bebd5696d7f265742f85df0cb
mukashovphoto <68531166+mukashovphoto@users.noreply.github.com>
2020-07-25 00:07:27 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = themeConfig => {
     nav: themeConfig.nav || [
       {
         text: 'Blog',
-        link: '/',
+        link: '/blog',
       },
       {
         text: 'Tags',
